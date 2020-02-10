5d25d11cc90ad371ef357597673fb2efbfc73172
grape11111 <2549107521@qq.com>
2020-02-10 21:18:56 +0800

    student login success

--- src/main/java/com/gdut/imis/campus/CampusApplication.java
+++ src/main/java/com/gdut/imis/campus/CampusApplication.java
@@ -1,9 +1,11 @@
 package com.gdut.imis.campus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.gdut.imis.campus.mapper")
 public class CampusApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/gdut/imis/campus/controller/IndexController.java
+++ src/main/java/com/gdut/imis/campus/controller/IndexController.java
@@ -1,14 +1,23 @@
 package com.gdut.imis.campus.controller;
 
+import com.gdut.imis.campus.mapper.StudentMapper;
+import com.gdut.imis.campus.model.Student;
+import com.gdut.imis.campus.model.StudentExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+
+    @Autowired
+    private StudentMapper studentMapper;
 
     @GetMapping("/")
     public String test(HttpServletRequest request){
@@ -30,9 +39,21 @@ public class IndexController {
         String username=request.getParameter("username");
         String pwd=request.getParameter("password");
         String type=request.getParameter("type");
-        if(username.equals("aaa") && pwd.equals("123") && type.equals("0")){
-            request.getSession().setAttribute("user",username);
+        StudentExample studentExample = new StudentExample();
+        studentExample.createCriteria()
+                .andAccountIdEqualTo(username);
+        List<Student> list=studentMapper.selectByExample(studentExample);
+        if (list.size()!=0) {
+            Student stu=list.get(0);
+            if( pwd.equals(stu.getPassword()) && type.equals(stu.getType().toString())){
+                request.getSession().setAttribute("user",stu);
+            }else{
+                System.out.println("密码错误"+stu.getPassword()+stu.getType());
+            }
+        }else{
+            System.out.println("该用户不存在！");
         }
+
         return "index";
     }
 
